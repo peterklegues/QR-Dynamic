4114979bbb1d5c4f9ed3f4b348115d2b765f41f9
[dyad] <peterklegues@gmail.com>
2025-07-14 20:14:44 -0300

    [dyad] Corrigindo erro de múltiplos filhos em QueryClientProvider - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-// import { TooltipProvider } from "@/components/ui/tooltip"; // Temporarily removed
+import { TooltipProvider } from "@/components/ui/tooltip"; // Re-importing TooltipProvider as it might be needed later
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
@@ -14,9 +14,9 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <Toaster />
-    <Sonner />
-    {/* <TooltipProvider> */}
+    <React.Fragment> {/* Wrap multiple children in a Fragment */}
+      <Toaster />
+      <Sonner />
       <BrowserRouter>
         <SessionContextProvider>
           <Routes>
@@ -27,7 +27,7 @@ const App = () => (
           </Routes>
         </SessionContextProvider>
       </BrowserRouter>
-    {/* </TooltipProvider> */}
+    </React.Fragment>
   </QueryClientProvider>
 );
 
